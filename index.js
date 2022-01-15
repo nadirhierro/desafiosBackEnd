5a655f72b9f2fbb534e90b5a611a61555815cc78
Nadir Hierro <nadir.hierro@gmail.com>
2022-01-15 16:33:25 -0300

    Desafío 2

--- index.js
+++ index.js
@@ -1,50 +1,44 @@
-class User {
-  constructor(name, lastName, books, pets) {
-    this.name = name;
-    this.lastName = lastName;
-    this.books = books;
-    this.pets = pets;
-  }
-  getFullName() {
-    return `The user name is ${this.name} ${this.lastName}`;
-  }
-  addPet(petName) {
-    this.pets.push(petName);
-  }
-  countPets() {
-    if (this.pets.length == 0) {
-      return `${this.name} does not have any pet`;
-    } else {
-      return `${this.name} ${
-        this.pets.length > 1
-          ? `has ${this.pets.length} pets`
-          : `have ${this.pets.length} pet`
-      }`;
-    }
-  }
-  addBook(bookName, author) {
-    this.books.push({ name: bookName, author: author });
-  }
-  getBookNames() {
-    let bookNames = this.books.map((book) => book.name);
-    return bookNames.join(" - ");
-  }
-}
+import Container from "./container.js";
 
-let nadir = new User(
-  `Nadir`,
-  `Hierro`,
-  [
-    { name: `El señor de las moscas`, author: `William Golding` },
-    { name: "Fundacion", author: "Isaac Asimov" },
-  ],
-  [`Sara`]
-);
+// productos sueltos y container de productos
+const product1 = {
+  title: "Lapicera",
+  price: 12.3,
+  thumbnail: "https://www.unsitio.com",
+};
+const product2 = {
+  title: "Lápiz",
+  price: 5.5,
+  thumbnail: "https://www.unsitio.com",
+};
+const product3 = {
+  title: "Cuaderno",
+  price: 12.45,
+  thumbnail: "https://www.unsitio.com",
+};
 
-console.log(nadir.getFullName());
-console.log(nadir.countPets());
-nadir.addPet(`Luna`);
-console.log(nadir.countPets());
-console.log(nadir.getBookNames());
-nadir.addBook(`En contra del método`, `Paul Feyerabend`);
-console.log(nadir.getBookNames());
+const products = new Container(`./products.txt`);
+
+// llamado de prueba para crear el txt con array vacío
+
+// products.write(JSON.stringify([], null, 2));
+
+// llamado de prueba para agregar objeto
+
+// products.save(product1);
+
+// llamado para getById
+
+// products.getById(5);
+
+// llamado para getAll
+
+// products.getAll();
+
+// llamado para deleteById
+
+// products.deleteById(2);
+
+// llamado para deleteAll
+
+// products.deleteAll();
